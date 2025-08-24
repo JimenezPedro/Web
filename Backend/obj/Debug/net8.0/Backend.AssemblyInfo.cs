a30327921eb4c4c43a38d4a8f54f577afab2fcd1
JimenezPedro <jimzampedro@gmail.com>
2025-08-24 18:35:49 +0200

    DbContext and db creation

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98a0355b116b2d7a5f7e7ae66374a2fcb7aab513")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b99046518b7c3f294cc83d20e0d4d1d3a1442f5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
